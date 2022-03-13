e2980a1d8d4a25e6ea8252b82c1d0d07521fc6a2
gabrield11 <gabrieldinizm03@gmail.com>
2022-03-13 14:26:21 -0300

    melhoração do front register

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -29,34 +29,34 @@
 
     ?>
 
-    <div class="mask d-flex align-items-center h-100 gradient-custom-3">
-        <div class="container h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-9 col-lg-7 col-xl-6">
+    <div class="mask d-flex align-items-center h-50 gradient-custom-3">
+        <div class="container py-4 h-25">
+            <div class="row g-0 d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-5">
                     <div class="card" style="border-radius: 15px;">
-                        <div class="card-body p-5">
+                        <div class="card-body p-4">
                             <h2 class="text-uppercase text-center mb-5">Criar uma conta</h2>
 
                             <form action="app/controllers/userController.php?action=register" method="POST">
 
-                                <div class="form-outline mb-4">
+                                <div class="form-outline mb-3">
                                     <label for="field_nome_cadastro" class="form-label">Nome</label>
-                                    <input name="field_nome_cadastro" type="text" class="form-control form-control-lg" />
+                                    <input name="field_nome_cadastro" type="text" class="form-control form-control-md" />
                                 </div>
 
-                                <div class="form-outline mb-4">
+                                <div class="form-outline mb-3">
                                     <label for="field_usuario_cadastro" class="form-label">Usuário</label>
-                                    <input name="field_usuario_cadastro" type=" text" class="form-control form-control-lg" />
+                                    <input name="field_usuario_cadastro" type=" text" class="form-control form-control-md" />
                                 </div>
 
-                                <div class="form-outline mb-4">
+                                <div class="form-outline mb-3">
                                     <label for="field_email_cadastro" class="form-label">Email</label>
-                                    <input name="field_email_cadastro" type="email" class="form-control form-control-lg" />
+                                    <input name="field_email_cadastro" type="email" class="form-control form-control-md" />
                                 </div>
 
-                                <div class="form-outline mb-4">
+                                <div class="form-outline mb-3">
                                     <label for="field_senha_cadastro" class="form-label" ">Senha</label>
-                                    <input name=" field_senha_cadastro" type="password" class="form-control form-control-lg" />
+                                    <input name=" field_senha_cadastro" type="password" class="form-control form-control-md" />
                                 </div>
 
                                 <div class="d-flex justify-content-center">
