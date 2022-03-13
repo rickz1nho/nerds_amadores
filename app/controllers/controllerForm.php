f35f720d45aee38418884c6194278151aac26116
rickz1nho <mhferreira28@gmail.com>
2022-03-13 02:44:07 -0300

    VOltar index

--- app/controllers/controllerForm.php
+++ app/controllers/controllerForm.php
@@ -1,7 +1,7 @@
 <?php
 
-    require_once __DIR__."/../../config.php";
     require_once __DIR__."/../repositories/UserRepository.php";
+    require_once __DIR__."/../../config.php";
 
 
     //roteamento
@@ -54,6 +54,6 @@
 
         $repository->deletePubli($publiId['publi']['id']);
 
-    }
-        ?>
+        header("location:" .BASE_URL. "/index.php?msg=publicacaodeletada");
 
+    }
