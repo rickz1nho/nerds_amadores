4cdc8be1ff81f59ec39227ec8477614a3607b78d
gabrield11 <gabrieldinizm03@gmail.com>
2022-03-13 18:30:25 -0300

    front index

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <script src="https://kit.fontawesome.com/0a30d20add.js" crossorigin="anonymous"></script>
 
     <!-- Codigo copiado-->
     <link href="assets/images/favicon/favicon.png" rel="icon">
-    <link href="https://fonts.googleapis.com/css?family=Exo+2:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i%7CRoboto:300,300i,400,400i,500,500i,700,700i,900,900i%7CWork+Sans:300,400,500,600,700,800,900" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Baloo+2:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i%7CRoboto:300,300i,400,400i,500,500i,700,700i,900,900i%7CWork+Sans:300,400,500,600,700,800,900" rel="stylesheet" type="text/css">
     <link href="assets/css/external.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="assets/revolution/css/settings.css">
@@ -39,10 +41,10 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container-fluid bg-dark">
         <div class="row">
             <div class="col">
-                <h1>Home page</h1>
+                <h1 class="text-white">Home page</h1>
 
             </div>
             <?php
@@ -52,8 +54,8 @@
             if (isset($_SESSION['usuario'])) {
             ?>
                 <div class="col mt-2">
-                    <p> Olá, <?= $_SESSION['usuario']['nome'] ?>
-                        @ <?= $_SESSION['usuario']['usuario'] ?></p>
+                    <p class="text-white"> Olá, <?= $_SESSION['usuario']['nome'] ?>
+                        @<?= $_SESSION['usuario']['usuario'] ?></p>
                 </div>
             <?php } ?>
 
@@ -62,18 +64,18 @@
             if (!isset($_SESSION['usuario'])) {
             ?>
                 <div class="col">
-                    <a class="btn btn-default" href='login_page.php'>Realizar login </a>
+                    <a class="btn btn-default text-white" href='login_page.php'>Realizar login </a>
 
-                    <a class="btn btn-default" href='cadastro_usuario.php'>Cadastre-se</a>
+                    <a class="btn btn-default text-white" href='cadastro_usuario.php'>Cadastre-se</a>
                 </div>
             <?php
             } else {
             ?>
                 <div class="col">
-                    <a class="btn btn-default" href='valida_login.php'>Meu perfil</a>
+                    <a class="btn btn-default text-white" href='valida_login.php'>Meu perfil</a>
                 </div>
 
-                <?php
+            <?php
             }
 
 
@@ -81,11 +83,11 @@
             $repository->view();
 
 
-            if(!empty($repository->getPubliId())){
-                ?>
+            if (!empty($repository->getPubliId())) {
+            ?>
                 <form action="app/controllers/controllerForm.php?action=delete" method="POST">
 
-                <input type="submit" value="DELETAR PUBLICACAO" onclick="return confirm('Tem certeza que deseja deletar essa publicacao?')">
+                    <button class="button-67" type="submit" onclick="return confirm('Tem certeza que deseja deletar essa publicacao?')">Deletar Publicação</button>
 
                 </form>
             <?php
